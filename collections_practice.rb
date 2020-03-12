54836b996cbbcb553488a932be11d33ec84f0db4
Marcos Rodriguez <m.rodrig.felix@gmail.com>
2020-03-12 06:16:29 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -47,8 +47,12 @@ end
 
 
 def find_a(array)
-  array.select do |word|
-    word.start_with?("a")
+  # array.select do |word|
+  #   word.start_with?("a")
+  # end
+
+  array.find_all do |word|
+    word.start_with("a")
   end
 end
 
